d0e3cd75a5f2227543ee477e91080d5dcb244352
messboy home <messboy000@gmail.com>
2014-11-23 12:24:31 +0800

    三種常用委派類型

---
+++ Csharp_in_Depth/Delegate_To_Lamda/Films.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Delegate_To_Lamda
+{
+    public class Films
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+}
--- Csharp_in_Depth/Delegate_To_Lamda/Program.cs
+++ Csharp_in_Depth/Delegate_To_Lamda/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,6 +27,37 @@ namespace Delegate_To_Lamda
             returnLength = x => x.Length;
 
             Console.WriteLine(returnLength("Hello"));
+
+            //*************************************************************
+
+            //Action是一種void的委派類型,搭配lambda
+            Action<Films> print = film => Console.WriteLine("Name:{0}, Year:{1}", film.Name, film.Year);
+
+            //Pridecate是一種回傳bool的委派類型
+            Predicate<Films> isCheck = film => film.Year >= 1980;
+
+            //電影列表
+            var films = new List<Films>
+            {
+                new Films {Name = "Jams", Year = 1975},
+                new Films {Name = "aams", Year = 1985},
+                new Films {Name = "bams", Year = 1985},
+                new Films {Name = "cams", Year = 1905},
+                new Films {Name = "Jams", Year = 1945},
+                new Films {Name = "Jams", Year = 1975},
+            };
+
+            //回圈列出
+            films.ForEach(print);
+            Console.WriteLine();
+
+            //篩選後列印
+            films.FindAll(isCheck).ForEach(print);
+            Console.WriteLine();
+
+            //排序
+            films.Sort((f1, f2) => f1.Name.CompareTo(f2.Name));
+            films.ForEach(print);
         }
     }
 }
