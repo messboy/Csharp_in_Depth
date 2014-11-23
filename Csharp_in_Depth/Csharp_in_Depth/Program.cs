44a28842573176b86e416bdffab58f4e1c52cd37
messboy home <messboy000@gmail.com>
2014-11-23 11:46:40 +0800

    Delegate_To_Lambda

---
+++ Csharp_in_Depth/Csharp_in_Depth/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_in_Depth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = Product.GetSampleProducts();
+            //C#2.0 使用委派取代額外實作IComparer 類別
+            products.Sort(delegate(Product  x, Product y) { return x.Name.CompareTo(y.Name); });
+            Print(products);
+
+            //C#3.0 使用Lambda 進行排序
+            products.Sort((x, y) => y.Name.CompareTo(x.Name));
+            Print(products);
+
+            //C#3.0 使用Lambda 擴展方法
+            foreach (Product product in products.OrderBy(p => p.Name))
+            {
+                Console.WriteLine(product);
+            }
+
+
+        }
+
+        private static void Print(List<Product> products)
+        {
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product);
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+    
+}
---
+++ Csharp_in_Depth/Delegate_To_Lamda/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_To_Lamda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Func是一個委派類型 <參數 參數...,最後一個是回傳型別>
+            Func<string, int> returnLength;
+
+            //1.以匿名方法開始
+            returnLength = delegate(string text) { return text.Length; };
+
+            //2.Lambda
+            returnLength = (string text) => { return text.Length; };
+
+            //3.單個表達是
+            returnLength = (text) => text.Length;
+
+            //4.不用刮號
+            returnLength = x => x.Length;
+
+            Console.WriteLine(returnLength("Hello"));
+        }
+    }
+}
